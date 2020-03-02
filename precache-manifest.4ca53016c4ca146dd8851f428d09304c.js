5153b526c8e463fa7355f17374ca5e7230cb3ed6
Yuliya312 <yu312liya@gmail.com>
2020-03-02 22:24:55 +0200

    Updates

---
+++ precache-manifest.4ca53016c4ca146dd8851f428d09304c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f599dbaa4798cc0fe79a8e7b738fa6a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "624edebaaa21af2a3ea6",
+    "url": "./static/css/main.e790e00f.chunk.css"
+  },
+  {
+    "revision": "1bf9d59d943f44d77c3b",
+    "url": "./static/js/2.1bf17507.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.1bf17507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624edebaaa21af2a3ea6",
+    "url": "./static/js/main.53a10f28.chunk.js"
+  },
+  {
+    "revision": "2e3a6f278567b71a3edc",
+    "url": "./static/js/runtime-main.5e540f5e.js"
+  }
+]);
